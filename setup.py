962f4c8a1d96d30c077965b6593f7fc3bc78c76b
Tomas D'Stefano <tomas_stefano@successoft.com>
2012-05-10 16:07:36 -0300

    Change setup.py to use setup tools and put the package name in the setup()

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-LOCAWEB_GATEWAY_VERSION = 'v0.0.1'
+LOCAWEB_GATEWAY_VERSION = 'v0.1.1'
 
-setup(
-    name='locaweb_gateway',
+setup(name='locaweb_gateway',
     version=LOCAWEB_GATEWAY_VERSION,
-    description='Locaweb gateway bindings',
-    author='Locaweb',
-    packages=[],
-    install_requires=['requests >= 0.8.8', 'simplejson']
+    description='',
+    author='locaweb',
+    packages=['locaweb_gateway'],
+    install_requires=['requests >= 0.8.8', 'simplejson'],
+    author_email = "saas-dev@locaweb.com.br",
+    license = "MIT license",
+    keywords = "locaweb gateway pagamento checkout",
+    url = "https://github.com/locaweb/locaweb-gateway-python",
+    include_package_data=True
 )
